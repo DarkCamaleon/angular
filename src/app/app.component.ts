9f3d8d9e3a69a6b4ecbdf1bc506d84107ec17ac8
Alejandro Venegas <alejandro.b.venegas.ch@gmail.com>
2022-09-08 16:53:32 -0400

    c4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template:`
+  // <h1>hola mundo</h1>
+  // ` ,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '01-bases';
+  title: string = 'contador app';
+  numero: number = 10;
+  base : number = 5;
+
+  // sumar(){
+  //   this.numero += 1;
+  // }
+
+  // restar(){
+  //   this.numero -= 1;
+  // }
+  acumular( valor:number ){
+    this.numero +=valor;
+  }
 }
+ 
